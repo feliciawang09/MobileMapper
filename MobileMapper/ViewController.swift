397dcf13de99912f4b91c6d1185c4ff72ac56ba3
Felicia 2019 <fwang9@3012-l29552-s.local>
2019-04-02 09:36:00 -0500

    Initial commit (map loading, current location tracked)

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var currentLocation: CLLocation!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
 
+    func locationmanager(_manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        currentLocation = locations[0]
+    }
 
 }
 
